162ce97c80722b395bb4f655cc3776d7ecd49519
jhon <susidko.2012@mail.ru>
2020-12-27 15:45:58 +0300

    js commit nachalo

---
+++ js/script.js
@@ -0,0 +1,118 @@
+"use strict";
+
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5` ) ;
+
+
+const user = 'jhon sina';
+
+alert(`Privet, ${user} `);
+
+
+let incr = 10,
+    decr = 10;
+
+/* incr++;
+decr--; */
+
+console.log(++incr);
+console.log(--decr);
+
+
+console.log(5%2);
+
+
+console.log(2*4 == 8);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//alert('Hello');
+
+//const result = confirm("Are you here ? ");
+//console.log(result);
+
+//const answer = prompt("Вам есть 18 ? " , "" ) ;
+//console.log(answer);
+/* 
+const answers = [];
+
+answers[0] = prompt('Как ваше имя?' , '');
+answers[1] = prompt('Как ваша фамилия?' , '');
+answers[2] = prompt('сколько вам лет ? ' , '');
+
+
+document.write(answers);
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* "use strict";
+
+a = 13;
+console.log(a);
+
+
+
+let number = 1;
+const leftBorderwidth = 5;
+
+number = 10 ;
+console.log(number);
+/* 
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+console.log(obj); */
+
+/* 
+
+console.log(name);
+var name = 'Artem';
+
+
+{
+    var result = 50; 
+}
+
+console.log(result);
+
+
+const obj = {
+    name: "sina" ,
+    age: 25 ,
+    isMarried: false
+};
+
+console.log(obj["name"]);
+
+let arr = ['plum.png' , 'orange.jpg' , 6 , 'apple.bmp' ,
+{} , [] ] ;
+console.log(arr[2])
+ */
